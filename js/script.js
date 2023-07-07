fd68578234fac6e1f70b5ae3fb560150151128e7
jpalvespinheiro <jpalvespinheiro@gmail.com>
2023-07-07 00:42:09 -0300

    Em desenvolvimento - parte final

--- js/script.js
+++ js/script.js
@@ -2,21 +2,59 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
 
 /*==================== scroll sections active link ====================*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href *=' + id + ']').classList.add('active')
+            });
+        };
 
-    /*==================== sticky navbar ====================*/
+    });
 
-    /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
 
+ScrollReveal({ 
+    //reset: true,
+    distance: '80px',
+    duration: 2000, 
+    delay: 200
+});
 
-/*==================== scroll reveal ====================*/
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
+ScrollReveal().reveal('.home-img, .servicos-container, .projetos-box, .contatos form', { origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .sobre-img', { origin: 'left'});
+ScrollReveal().reveal('.home-content p, .sobre-content', { origin: 'right'});
+
 
 
 /*==================== typed js ====================*/
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Desenvolvedor FullStack','Desenvolvedor Mobile', 'Designer gráfico'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
