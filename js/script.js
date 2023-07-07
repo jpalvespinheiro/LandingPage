0202e73731fc3f9d8577ba01572fbeb60784c06f
Joao Alves <jpalvespinheiro@gmail.com>
2023-07-07 13:57:20 -0300

    mudanças novas

--- js/script.js
+++ js/script.js
@@ -6,36 +6,43 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 };
 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a [href *=' + id + ']').classList.add('active')
-            });
-        };
-
-    });
-
-    let header = document.querySelector('header');
-
-    header.classList.toggle('sticky', window.scrollY > 100);
-
+navbar.onclick = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
 
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    // sections.forEach(sec => {
+    //     let top = window.scrollY;
+    //     let offset = sec.offsetTop - 150;
+    //     let height = sec.offsetHeight;
+    //     let id = sec.getAttribute('id');
+
+    //     if(top >= offset && top < offset + height) {
+    //         navLinks.forEach(links => {
+    //             links.classList.remove('active');
+    //             document.querySelector('header nav a [href *=' + id + ']').classList.add('active');
+    //         });
+    //     };
+
+    // });
+
+    // let header = document.querySelector('header');
+
+    // header.classList.toggle('sticky', window.scrollY > 100);
+
+    // menuIcon.classList.remove('bx-x');
+    // navbar.classList.remove('active');
+};
+
 ScrollReveal({ 
     //reset: true,
-    distance: '80px',
+    distance: '10px',
     duration: 2000, 
     delay: 200
 });
